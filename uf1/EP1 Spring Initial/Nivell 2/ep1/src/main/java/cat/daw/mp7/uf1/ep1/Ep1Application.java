11b49cc39dc5e74ddaa71addf25fe27a0da62bf7
Marcello Krell <mkrell20@ilg.cat>
2022-02-07 22:08:22 +0000

    done

---
+++ uf1/EP1 Spring Initial/Nivell 2/ep1/src/main/java/cat/daw/mp7/uf1/ep1/Ep1Application.java
@@ -0,0 +1,13 @@
+package cat.daw.mp7.uf1.ep1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ep1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ep1Application.class, args);
+	}
+
+}
---
+++ uf1/EP1 Spring Initial/Nivell 2/ep1/src/test/java/cat/daw/mp7/uf1/ep1/Ep1ApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.daw.mp7.uf1.ep1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ep1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
